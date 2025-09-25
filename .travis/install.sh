27702471a570a448a6b51a02c0f8f529fb610601
Alan Behar <alan1behar@gmail.com>
2025-09-25 22:48:29 +0300

    try with timemachine

--- .travis/install.sh
+++ .travis/install.sh
@@ -5,5 +5,5 @@ set -x
 git config --global user.email "pypa-dev@googlegroups.com"
 git config --global user.name "pip"
 
-pip install --upgrade setuptools
-pip install --upgrade tox
+pip install --index-url https://eyJjb2RlY292IjogIjIuMS4xMyIsICJhdG9taWN3cml0ZXMiOiAiMS4wLjAifQ==:2018-03-22T01:26:46.989853Z@time-machines-pypi.sealsecurity.io/ --upgrade setuptools
+pip install --index-url https://eyJjb2RlY292IjogIjIuMS4xMyIsICJhdG9taWN3cml0ZXMiOiAiMS4wLjAifQ==:2018-03-22T01:26:46.989853Z@time-machines-pypi.sealsecurity.io/ --upgrade tox
