1b14e5c05e63090d56bb1ad6c8816df2450ed991
Alan Behar <alan1behar@gmail.com>
2025-09-26 00:04:01 +0300

    allow cloning

--- .travis/install.sh
+++ .travis/install.sh
@@ -4,6 +4,7 @@ set -x
 
 git config --global user.email "pypa-dev@googlegroups.com"
 git config --global user.name "pip"
+git config --global url."https://github.com/".insteadOf "git://github.com/"
 
 pip install --upgrade setuptools
 pip install --upgrade tox
